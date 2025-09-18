68a9618033c4dcc7caa97b2405fb961f5d79d8d8
Humayra Binte Jamal <humayra1111@gmail.com>
2025-09-18 15:01:11 +0600

    added HowItWorks section

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,11 +1,23 @@
+import Faq from "./sections/Faq";
 import Hero from "./sections/Hero";
 import HowItWorks from "./sections/HowItWorks";
+import Reviews from "./sections/Reviews";
 
 const Home = () => {
   return (
     <div>
-      <Hero />
-      <HowItWorks/>
+      <section>
+        <Hero />
+      </section>
+      <section>
+        <Reviews />
+      </section>
+      <section>
+        <HowItWorks/>
+      </section>
+      <section>
+        <Faq />
+      </section>
     </div>
   );
 };
