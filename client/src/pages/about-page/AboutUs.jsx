31990faaf0f7b5d5a28c43c5179d32026a8a6ab6
Aasif-Shahriar <asifshahriar247@gmail.com>
2025-09-18 16:08:05 +0600

    Added: our mission section in the about page

--- client/src/pages/about-page/AboutUs.jsx
+++ client/src/pages/about-page/AboutUs.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import TopSection from '../home/sections/TopSection';
+import React from "react";
+import TopSection from "./sections/TopSection";
+import OurMission from "./sections/OurMission";
 
 const AboutUs = () => {
-    return (
-        <section className='bg-white min-h-screen max-w-7xl mx-auto mt-16'>
-           <TopSection />
-        </section>
-    );
+  return (
+    <section className="bg-white min-h-screen max-w-7xl mx-auto mt-16">
+      <TopSection />
+      <OurMission />
+    </section>
+  );
 };
 
 export default AboutUs;
