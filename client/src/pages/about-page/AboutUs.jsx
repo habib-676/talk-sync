c01cd33b67ebe45b9ef0973f02f87f241d5f1a2f
Aasif-Shahriar <asifshahriar247@gmail.com>
2025-09-18 17:12:30 +0600

    Added: why we built talksync section in the about page

--- client/src/pages/about-page/AboutUs.jsx
+++ client/src/pages/about-page/AboutUs.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import TopSection from "./sections/TopSection";
 import OurMission from "./sections/OurMission";
+import WhyWeBuilt from "./sections/WhyWeBuilt";
+import MeetTheTeam from "./sections/MeetTheTeam";
+import OurValues from "./sections/OurValues";
 
 const AboutUs = () => {
   return (
     <section className="bg-white min-h-screen max-w-7xl mx-auto mt-16">
       <TopSection />
       <OurMission />
+      <WhyWeBuilt />
+      <MeetTheTeam />
+      <OurValues />
     </section>
   );
 };
