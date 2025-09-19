c66104d3f6f67ccd361c607647f1275e779338fe
Aasif-Shahriar <asifshahriar247@gmail.com>
2025-09-19 12:16:07 +0600

    Add: company logo component

--- client/src/components/shared/navbar-components/Navbar.jsx
+++ client/src/components/shared/navbar-components/Navbar.jsx
@@ -2,18 +2,20 @@ import React from "react";
 import MobileNav from "./MobileNav";
 import AuthButtons from "./auth-buttons/AuthButtons";
 import DesktopNav from "./Desktopnav";
-import { Link } from "react-router";
+import TalkSyncLogo from "../../logo/TalkSyncLogo";
+import LogoSmallDevice from "../../logo/LogoSmallDevice";
 
 const Navbar = () => {
   return (
     <nav className="fixed top-0 left-0 w-full h-16 bg-base-100 text-base-content shadow-md">
       <div className="maximum-w px-4 h-full mx-auto flex items-center justify-between">
         {/* logo ⬇ */}
-        <Link to={"/"} className="flex items-center gap-2">
-          <div className="flex items-center gap-2">
-            <h2 className="text-2xl text-primary font-bold">TalkSync</h2>
-          </div>
-        </Link>
+        <div className="hidden sm:block">
+          <TalkSyncLogo />
+        </div>
+        <div className="sm:hidden">
+          <LogoSmallDevice />
+        </div>
 
         {/* sidebar and nav-links  for small devices ⬇ */}
         <MobileNav />
