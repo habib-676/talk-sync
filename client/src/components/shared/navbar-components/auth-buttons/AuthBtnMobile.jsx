f6fc53c5e1c6cd8e9cd15bb7a207b00e423b47c4
Aasif-Shahriar <asifshahriar247@gmail.com>
2025-10-15 16:44:33 +0600

    Update: mobile menu UI

--- client/src/components/shared/navbar-components/auth-buttons/AuthBtnMobile.jsx
+++ client/src/components/shared/navbar-components/auth-buttons/AuthBtnMobile.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { Link } from "react-router";
-import toast from "react-hot-toast";
 import useAuth from "../../../../hooks/useAuth";
+import toast from "react-hot-toast";
+import { Link } from "react-router";
 
-const AuthButtons = () => {
+const AuthBtnMobile = () => {
   const { user, logOut } = useAuth();
 
   const handleLogout = () => {
@@ -18,21 +18,9 @@ const AuthButtons = () => {
   };
 
   return (
-    <div className="hidden lg:flex gap-4 items-center">
+    <div className="lg:flex gap-4 items-center">
       {user ? (
         <div className="flex items-center gap-4">
-          {/* Avatar or default icon that links to Profile */}
-          <Link to="/profile">
-            <div className="avatar cursor-pointer">
-              <div className="w-12 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
-                <img
-                  src={user.photoURL || "/default-avatar.png"} // fallback if no photo
-                  alt={user.displayName || "User"}
-                />
-              </div>
-            </div>
-          </Link>
-
           {/* Logout button */}
           <button
             onClick={handleLogout}
@@ -49,7 +37,7 @@ const AuthButtons = () => {
             </button>
           </Link>
           <Link to="/auth/register">
-            <button className="bg-primary text-white font-medium px-4 py-2 rounded cursor-pointer transform hover:-translate-y-1 transition-all duration-300">
+            <button className="btn btn-outline text-primary font-medium px-4 py-2 rounded cursor-pointer transform hover:-translate-y-1 transition-all duration-300">
               Sign Up
             </button>
           </Link>
@@ -59,4 +47,4 @@ const AuthButtons = () => {
   );
 };
 
-export default AuthButtons;
+export default AuthBtnMobile;
